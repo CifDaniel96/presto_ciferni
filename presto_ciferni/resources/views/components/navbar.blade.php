85028c9974bf6d58dd7d9abe2dbd338a89fcb058
Daniel Ciferni <cif.daniel96@gmail.com>
2026-05-14 11:28:56 +0200

    User Story 10 completata

--- presto_ciferni/resources/views/components/navbar.blade.php
+++ presto_ciferni/resources/views/components/navbar.blade.php
@@ -77,6 +77,16 @@
                     </li>
                 @endauth
             </ul>
+            <form class="d-flex ms-auto" role="search" action="{{ route('article.search')}}" method="GET">
+                <div class="input-group">
+                    <input type="search" name="query" class="form-control" placeholder="Search" aria-label="search">
+                    <button type="submit" class="input-group-text btn btn-outline-success"
+                        id="basic-addon2">
+                        Search
+                    </button>
+                </div>
+            </form>               
+            </div>
         </div>
     </div>
 </nav>
--- presto_ciferni/routes/web.php
+++ presto_ciferni/routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 
 // PublicController
 Route::get('/', [PublicController::class, 'homepage'])->name('homepage');
+Route::get('/search/article', [PublicController::class, 'searchArticles'])->name('article.search');
 
 // ArticleController
 Route::get('/create/article', [ArticleController::class, 'create'])->name('create.article');
