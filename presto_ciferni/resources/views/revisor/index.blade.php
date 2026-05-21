a76e63f7c815d5ee7bee14b82fee9040737852e8
Daniel Ciferni <cif.daniel96@gmail.com>
2026-05-21 18:14:10 +0200

    User Story 6 completata

--- presto_ciferni/resources/views/revisor/index.blade.php
+++ presto_ciferni/resources/views/revisor/index.blade.php
@@ -31,7 +31,7 @@
                             @foreach ($article_to_check->images as $key => $image)
                                 <div class="col-6 col-md-4 mb-4 text-center">
                                     <img 
-                                        src="{{ Storage::url($image->path) }}" 
+                                        src="{{ $image->getUrl(300, 300) }}" 
                                         class="img-fluid rounded shadow"
                                         alt="Immagine {{ $key + 1 }} dell'articolo {{ $article_to_check->title }}"
                                     >
